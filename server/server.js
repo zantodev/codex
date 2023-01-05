9425445b637ddbf24f50af4227e7fb4b31873972
zantodev <user25@gmx.com>
2023-01-04 19:53:45 -0800

    first commit

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.Completion.create({
+    const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
